c2f484b74ea6f40c94383f84e0e69308bca8762b
Shankar <shankarmylesamy@gmail.com>
2026-03-25 08:12:04 +0530

    warnings-fixed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import {useEffect, useState} from "react";
 import './Header.css';
 
 export function Header() {
-
-    const [ navBarLinks, setNavBarLinks] = useState(true);
-    const listItems = { display : navBarLinks ? "block" : "none" };
-
-    useEffect( function() {
-        console.log("Current navbar " + navBarLinks);
-    },[navBarLinks])
     
     return (
         <header className="header">
             <p className="header-name">Shankar</p>
-            <ul className="header-nav-links" style={listItems}>
+            <ul className="header-nav-links">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#projects">Projects</a></li>
